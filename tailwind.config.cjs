1a622e3eaba4950cecc8e3d51987d726560f5a62
Piotr Mrożek <dnadesigned@gmail.com>
2021-07-10 23:02:59 +0200

    Add basic theme
    Added site's basic theme: colors, font style and sizes. Also added
    a new text banner.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,28 @@ module.exports = {
     fontFamily: {
       mono: ['ibmvga8', 'monospace'],
     },
+    colors: {
+      'transparent': 'transparent',
+      'black': 'var(--color-black)',
+      'blue': 'var(--color-blue)',
+      'green': 'var(--color-green)',
+      'cyan': 'var(--color-cyan)',
+      'red': 'var(--color-red)',
+      'magenta': 'var(--color-magenta)',
+      'brown': '#var(--color-brown)',
+      'light-gray': 'var(--color-light-gray)',
+      'dark-gray': 'var(--color-dark-gray)',
+      'light-blue': 'var(--color-light-blue)',
+      'light-green': 'var(--color-light-green)',
+      'light-cyan': 'var(--color-light-cyan)',
+      'light-red': 'var(--color-light-red)',
+      'light-magenta': 'var(--color-light-magenta)',
+      'yellow': 'var(--color-yellow)',
+      'white': 'var(--color-white)',
+    },
+    fontSize: {
+      'base': ['1rem', '1rem'],
+    },
     extend: {},
   },
   variants: {
